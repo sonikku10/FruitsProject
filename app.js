2f0311fc7e9472f97c33a337ed43bb1134d967a5
sonikku10 <sonikku10@msn.com>
2020-10-05 18:50:50 -0400

    First Commit: Switch MongoDB driver to Mongoose Driver. Add fruits/person via JS.

---
+++ app.js
@@ -0,0 +1,116 @@
+//jshint esversion:6
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useUnifiedTopology: true , useNewUrlParser: true });
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit. Can't really hate on them."
+});
+
+//fruit.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+  name: "Sara",
+  age: 26
+})
+
+//person.save();
+
+const kiwi = new Fruit ({
+  name: "Kiwi",
+  rating: 10,
+  review: "The best fruit!"
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  rating: 4,
+  review: "Too sour for me."
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 2,
+  review: "I don't like minions."
+});
+
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved all the fruits to fruitsDB");
+//   }
+// });
+
+
+Fruit.find(function(err, fruits) {
+  if (err) {
+    console.log(err);
+  } else {
+    //console.log(fruits);
+    fruits.forEach(function(fruit) {
+      console.log(fruit.name);
+    })
+  }
+});
+
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+    {
+      name: "Apple",
+      score: 8,
+      review: "Great Fruit!"
+    },
+    {
+      name: "Orange",
+      score: 6,
+      review: "Too Florida for me..."
+    },
+    {
+      name: "Banana",
+      score: 3,
+      review: "I don't like minions."
+    }
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits)
+    callback(fruits);
+  });
+}
